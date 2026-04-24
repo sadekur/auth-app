09286c64107cf1a3da51801f53cb460af52cde67
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-04-24 22:52:50 +0600

    Update login/ registration system ready to deploy

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 import Cookies from "js-cookie";
 
 export default function LoginPage() {
